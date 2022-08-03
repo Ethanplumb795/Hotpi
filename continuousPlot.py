4bf107968e8f630aef09f5d09983c123ba5977d6
Ethan Plumb <ethanplumb795@gmail.com>
2022-08-02 17:41:15 -0700

    Added keyboard interrupt, updated requirements to continuousPlot

--- continuousPlot.py
+++ continuousPlot.py
@@ -1,7 +1,8 @@
 import time
 import spidev
 from pynput import keyboard
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import numpy
 
 spi1 = spidev.SpiDev(1, 0)
 spi1.max_speed_hz = 5000000 # Guessing from the datasheet max sck freq
@@ -9,6 +10,25 @@ spi1.bits_per_word = 8
 
 global stop
 stop = False
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+def on_press(key):
+    global stop
+    if key.char == 'q':
+        stop = True
+        print()
+
+def make_plot():
+    dataFile = open("temperatureData.csv", "r")
+    data = dataFile.read()
+    dataFile.close()
+    print(data)
+
+    x = []
+    y = []
+    ax.plot(x, y)
 
 def read_adc(adc_ch, vref = 3.3):
     # Make sure ASC channel is 0 or 1:
@@ -39,6 +59,10 @@ def read_adc(adc_ch, vref = 3.3):
 
     return temp
 
+# Create the keyboard listener thread
+listener = keyboard.Listener( on_press=on_press )
+listener.start()
+
 # Report the channel 0 and channel 1 voltages to the terminal
 try:
     print("Writing to temperatureData.csv")
@@ -48,17 +72,23 @@ try:
     f.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()))
     f.close()
 
+    count = 0
+
     while not stop:
         adc_0 = read_adc(0)
         print(adc_0, "degrees C")
+
         strout = str(count) + ' ' + str(adc_0) + '\n'
-        print("strout =", strout)
-        f = open("timeVsTemp.txt", "a")
+        f = open("temperatureData.csv", "a")
         f.write(strout)
         f.close()
+
         count += 1
+
         if not stop:
-            time.sleep(0.5)
+            time.sleep(1)
+
+    make_plot()
 
 finally:
     spi1.close()
