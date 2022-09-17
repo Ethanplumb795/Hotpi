5dd2330fcfb733907462e562ce87b432d436c5d3
Ethan Plumb <ethan.plumb@keysight.com>
2022-09-16 18:25:48 -0700

    Trying to send message and recieve output from device

--- hotpid/src/main.rs
+++ hotpid/src/main.rs
@@ -1,5 +1,14 @@
 use rppal::spi;
 
 fn main() {
-    let spi0 = spi::Spi::new(spi::Bus::Spi0, spi::SlaveSelect::Ss0, 5000000, spi::Mode::Mode0);
+    let spi0 = spi::Spi {
+        bus: spi::Bus::Spi0,
+        slave_select: spi::SlaveSelect::Ss0,
+        clock_speed: 5000000,
+        mode: spi::Mode::Mode0,
+    };
+
+    let mut buf: [u8; 16] = [0; 16];
+
+    spi0.read(&mut buf);
 }
