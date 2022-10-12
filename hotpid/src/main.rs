26e4054acb8544f6465b0f066e20b9f26634a0a1
Ethan Plumb <ethan.plumb@keysight.com>
2022-10-12 11:28:24 -0700

    Set frequency

--- hotpid/src/main.rs
+++ hotpid/src/main.rs
@@ -5,7 +5,8 @@ use rppal::system::DeviceInfo;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // NOTE: in Python we used bus 1, device 0
-    let spi = Spi::new(Bus::Spi1, SlaveSelect::Ss0, 16_000_000, Mode::Mode0)?;
+    // Speed was 5_000_000
+    let spi = Spi::new(Bus::Spi1, SlaveSelect::Ss0, 5_000_000, Mode::Mode0)?;
 
     println!("Testing SPI on a {}.", DeviceInfo::new()?.model());
 
