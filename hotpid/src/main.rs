7d8911c838bb040d1a3da5fb9f8dcccd74703644
Ethan Plumb <ethan.plumb@keysight.com>
2022-10-12 11:25:54 -0700

    Changed spi device to the one we used to use

--- hotpid/src/main.rs
+++ hotpid/src/main.rs
@@ -4,7 +4,8 @@ use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 use rppal::system::DeviceInfo;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 16_000_000, Mode::Mode0)?;
+    // NOTE: in Python we used bus 1, device 0
+    let spi = Spi::new(Bus::Spi1, SlaveSelect::Ss0, 16_000_000, Mode::Mode0)?;
 
     println!("Testing SPI on a {}.", DeviceInfo::new()?.model());
 
