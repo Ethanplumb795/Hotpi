54f1186e838ffefc5153a23285ac3c873df8ba04
Ethan Plumb <ethanplumb795@gmail.com>
2022-11-21 10:04:20 -0700

    Not sure what changed

--- hotpid/src/main.rs
+++ hotpid/src/main.rs
@@ -1,11 +1,12 @@
 // TODO: Frequency of measurements
 //       Duration
-//       Number of averages
+//       Number of averages: done
 //       Start Time
 //       Save to CSV
 // Later: Initiate measurement experiment from a skeletonized flask web app
 
 use std::error::Error;
+use std::{thread, time};
 
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 use rppal::system::DeviceInfo;
@@ -25,35 +26,24 @@ fn take_measurement(spi:&Spi) -> Measurement {
         Ok(bytes) => bytes,
         Err(error) => panic!("Problem transferring to spi: {:?}", error),
     };
-    println!("Transfer Result: {}", num_bytes_transferred);
-
-    // Testing measurement
-    print!("Read buffer:\n");
-    for c in read_buffer {
-        print!("{:b}: ", c);
-        for i in (0..8).rev() {
-            print!("{}", (c & (1 << i)) >> i);
-        }
-        println!()
-    }
 
     let oc_fault:bool = (read_buffer[3] & 0b1) == 0b1;
     let scg_fault:bool = (read_buffer[3] & 0b10) == 0b10;
     let scv_fault:bool = (read_buffer[3] & 0b100) == 0b100;
     let fault:bool = (read_buffer[2] & 0b1) == 0b1;
-    println!("fault:{} ocf:{} scg:{} scv:{}", fault, oc_fault, scg_fault, scv_fault);
+    if fault || oc_fault || scg_fault || scv_fault {
+        println!("fault:{} ocf:{} scg:{} scv:{}", fault, oc_fault, scg_fault, scv_fault);
+    }
 
     // Bit 0 is lsb, buffer 0 is most significant buffer
     let mut temp_12:u16 = 0;
     temp_12 |= (read_buffer[2] as u16) << 4;
     temp_12 |= (read_buffer[3] as u16 & 0b11110000) >> 4;
-    println!("12 bit temp data: {:b}", temp_12);
 
     // Bit 0 is lsb, buffer 0 is most significant buffer
     let mut temp_14:u16 = 0;
     temp_14 |= (read_buffer[0] as u16) << 6;
     temp_14 |= (read_buffer[1] as u16 & 0b11111100) >> 2;
-    println!("14 bit temp data: {:b}", temp_14);
 
     let mut therm_temp_12:f32 = 0.0;
     for bit in 0..11 {
@@ -65,7 +55,6 @@ fn take_measurement(spi:&Spi) -> Measurement {
     if (temp_12 & 0b100000000000) == 0b100000000000 {
         therm_temp_12 *= -1.0;
     }
-    println!("Board temperature = {}", therm_temp_12);
 
     let mut therm_temp_14:f32 = 0.0;
     for bit in 0..13 {
@@ -77,7 +66,6 @@ fn take_measurement(spi:&Spi) -> Measurement {
     if (temp_14 & 0b10000000000000) == 0b10000000000000 {
         therm_temp_14 *= -1.0;
     }
-    println!("Thermocouple temperature = {}", therm_temp_14);
 
     let measurement = Measurement {
         board: therm_temp_12,
@@ -99,6 +87,7 @@ fn avg_measurement(n:u8, spi:&Spi) -> Measurement {
         let tmp_measurement = take_measurement(&spi);
         board.push(tmp_measurement.board);
         couple.push(tmp_measurement.couple);
+        thread::sleep(time::Duration::from_millis(10));
     }
 
     // Take average of measurements
@@ -115,6 +104,20 @@ fn avg_measurement(n:u8, spi:&Spi) -> Measurement {
 
     // Return avg measurement
     avg
+}
+
+fn meas_over_time(duration:u32, freq:f32, num_avg:u8, spi:&Spi) {
+    // Duration measured in seconds -> max duration = 49710.26963 days
+    // freq measured in hz
+    if freq <= 0 {
+        panic!("[ERROR] frequency must be a positive value.");
+    }
+    if duration == 0 {
+        let m = take_measurement(spi);
+    }
+    else {
+        // For the selected duration, set up an interrupt that will make the avg measurements
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -135,7 +138,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Test avg measurement
     let avg_ten = avg_measurement(10, &spi);
-    println!("Board average: {}. Thermocouple average: {}.", avg_ten.board, avg_ten.couple);
+    println!("\nBoard average: {}. Thermocouple average: {}.", avg_ten.board, avg_ten.couple);
 
     Ok(())
 }
